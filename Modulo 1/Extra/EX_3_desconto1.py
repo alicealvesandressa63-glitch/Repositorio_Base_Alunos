e93ebaca7b3bae0a1acf9f007e7c14804e66b454
alicealvesandressa63-glitch <alicealvesandressa63@gmail.com>
2025-09-11 08:55:19 -0300

    Add files via upload

---
+++ Modulo 1/Extra/EX_3_desconto1.py
@@ -0,0 +1,19 @@
+# Escreva um programa que pede ao usuário o preço de um produto e o valor de desconto em % e depois informe qual será o valor do desconto.
+# Dica: 
+# use a fórmula 
+# desconto = preco * (porcentagem / 100) 
+# para calcular o valor do desconto 
+
+# OUTPUT ESPERADO:
+
+# Qual o preço do produto? 300
+# Qual a porcentagem de desconto? 10
+# O produto que custa R$300.0 terá R$30.0 de desconto.
+
+# ------------------------------------------ ESCREVA SEU CÓDIGO ABAIXO -----------------------------------------------------------
+
+preco = float(input('Qual o valor do produto? '))
+porcentagem = float(input('Qual o valor da poercentagem que o valor terá? '))
+valor_desconto = float(preco*(porcentagem/100))
+# print('o produto que custa R$',preco,'terá',valor_desconto,'de desconto. ')
+print(f'o produto que custa R${preco} terá {valor_desconto} de desconto.')
---
+++ Modulo 1/Extra/EX_4_desconto2.py
@@ -0,0 +1,16 @@
+# Faça uma atualização no código do exercício anterior, agora o programa deve exibir o nome do produto, o valor do desconto e o valor final do produto.
+
+# OUTPUT ESPERADO:
+
+# Produto: FIAT TORO
+# Preço: 200000
+# Porcentagem de desconto: 15
+# O FIAT TORO com 15.0% de desconto custará R$ 170000.0
+
+# ------------------------------------------ ESCREVA SEU CÓDIGO ABAIXO -----------------------------------------------------------
+
+produto = input('Produto: ')
+preco = float(input('Preço:'))
+porcentagem = float(input('Porcentagem de desconto: '))
+valor_desconto = preco*(porcentagem/100)
+print (f'{produto} com {porcentagem}% de desconto custará {preco-valor_desconto}')
